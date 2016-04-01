a89762b3fb8d0f91c6aca878d08da53b3555c256
Nick Lyman <nick.lyman@gmail.com>
2016-04-01 09:30:45 -0700

    Developing the business logic in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,37 @@
 //business logic
+    var valid = function(validNumber) {
+      if ((number >= 1) && (number !== NaN)) {
+      return true;
+      alert("That is a valid number.");
+      } else {
+      return alert("Please enter a number greater than 0.");
+      }
+    };
 
-
-
+    var pingPong = function(number) {
+      for (var index = 0; index <= userNumber; index += userNumber) {
+        if (userNumber % 15 === 0) {
+          $(".output").append("<li>" + "PINGPONG!" + "</li>");
+      } if (userNumber % 5 === 0) {
+          $(".output").append("<li>" + "PONG!" + "</li>");
+      } if (userNumber % 3 === 0) {
+          $(".output").append("<li>" + "PING!" + "</li>");
+      } else {
+          $(".output").append("<li>" + userNumber + "</li>");
+        }
+      }
+    }
 
 
 //user interface logic
 $(document).ready(function() {
-  $("form#leap-year").submit(function(event) {
-    event.preventDefault();
+  $("form#number-form").submit(function(event) {
+    event.preventDefalit();
+
+    var userNumber = $(parseInt("#value").val());
+    console.log(userNumber);
+
+    $("#result").show();
+    $(".result").text(pingPong);
+  });
+});
