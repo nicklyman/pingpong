2a59402b8adc937647346fd6bc6473fe49fbc90b
Nick Lyman <nick.lyman@gmail.com>
2016-04-01 16:33:08 -0700

    Added styling with CSS and Bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,30 @@
 //business logic
-    var valid = function(validUserNumber) {
-      if ((validNumber >= 1) && (validNumber !== NaN)) {
-        return true;
-      } else {
-        return false;
-        alert("Please enter a number greater than 0.");
-      }
-    };
-
     var pingPong = function(userNumber) {
-      // if (true);
+      alert("hey");
       for (var index = 0; index <= userNumber; index += 1) {
-        if ((index > 0) && (index % 15 === 0)) {
-          $(".output").append("<li>" + "PINGPONG!" + "</li>");
-      } else if ((index >0) && (index % 5 === 0)) {
-          $(".output").append("<li>" + "PONG!" + "</li>");
-      } else if ((index >0) && (index % 3 === 0)) {
-          $(".output").append("<li>" + "PING!" + "</li>");
-      } else if (index > 0) {
-          $(".output").append("<li>" + (index) + "</li>");
+        if ((index <= 0) || (isNaN(index))) {
+          alert("Please enter a valid number greater than 0.");
+        } else if ((index > 0) && (index % 15 === 0)) {
+            $(".output").append("<li>" + "PINGPONG!" + "</li>")
+        } else if ((index > 0) && (index % 5 === 0)) {
+            $(".output").append("<li>" + "PONG!" + "</li>")
+        } else if ((index > 0) && (index % 3 === 0)) {
+            $(".output").append("<li>" + "PING!" + "</li>")
+        } else if (index > 0) {
+            $(".output").append("<li>" + (index) + "</li>")
         }
       }
-    }
+    };
 
 
 //user interface logic
 $(document).ready(function() {
-  $("form#number-form").submit(function(event) {
+  $("form#numberForm").submit(function(event) {
     event.preventDefault();
 
-    var validNumber = parseInt($("input#value").val());
-    var output = pingPong(validNumber);
+    var userNumber = parseInt($("input#value").val());
+    var output = pingPong(userNumber);
 
     $(".output").show();
-    // $("#value").text(validNumber)
-    // $(".result").text(pingPong);
   });
 });
